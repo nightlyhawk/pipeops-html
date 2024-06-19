1c42e7fd58e1faf8bf8546825f12229bcc6f80ae
Senanyo Benwari <benwarisenanyo@yahoo.com>
2024-06-19 11:51:07 +0100

    updateBackgrounColor

--- frontend/src/pages/style.js
+++ frontend/src/pages/style.js
@@ -6,6 +6,7 @@ export const Wrapper = styled.section`
     display: grid;
     grid-template-columns: 250px 1fr 1fr;
     grid-template-rows: 100px 1fr 1fr;
+    background-color: var(--primary);
 `
 export const OutletWrapper = styled.section`
     grid-column: 2 / 4;
